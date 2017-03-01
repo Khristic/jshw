948e8eadcfec9b61cd099450e3817c72fdd9496f
rhbcnz1991 <khristya2018@gmail.com>
2017-03-02 00:10:50 +0200

    initial commit

---
+++ HW7.js
@@ -0,0 +1,48 @@
+ 'use strict';
+
+// 1. Найти и покрасить последний span перед закрывающим тегом body в красный цвет.
+//var b=0;
+//var a = document.body.getElementsByTagName("span");
+//console.log(a);
+//b = a[a.length-1].style='color: red';
+
+
+
+//2.Только "Text 8" - зеленого цвета
+//var b=0;
+//var a = document.body.getElementsByTagName("span");
+//console.log(a);
+// b = a[a.length-2].style='color: green';
+
+
+//3. У ссылок только в первом списке убрать подчеркивания (во втором списке есть) и заменить текст на их порядковый номер
+
+//var ul = document.body.getElementsByTagName("ul");
+//var a = ul[0].getElementsByTagName("a");
+//console.log(a);
+//for (var i=0; i<a.length; i++){
+	//a[i].style='text-decoration: none';
+	//a[i].innerHTML=i+1;
+//}
+
+
+
+
+// 4."<h3>List</h3>" - синий фон
+//var a = document.body.getElementsByTagName("h3");
+//a[0].style = 'background: blue';
+
+
+// 5. Добавить еще один li в конец второго списка.
+//var a = document.body.getElementsByTagName("ul");
+//console.log(a);
+//var b = document.createElement('li');
+//b.innerHTML = "new li";
+//a[0].appendChild(b);
+
+
+
+
+
+
+
